908cb2a3182d571f67ce15105c02a2d57c9b2c2d
JulioC090 <juliocziviani@gmail.com>
2025-09-06 00:26:20 -0300

    feat: update sitemap and robots to use nextjs special route handler

---
+++ src/app/robots.ts
@@ -0,0 +1,22 @@
+import type { MetadataRoute } from 'next';
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: [
+      {
+        userAgent: 'Googlebot',
+        allow: ['/'],
+        disallow: ['/private/', '/admin/', '/404', '/500'],
+      },
+      {
+        userAgent: ['Applebot', 'Bingbot', 'Googlebot'],
+        allow: ['/'],
+      },
+      {
+        userAgent: ['AhrefsBot', 'SemrushBot'],
+        crawlDelay: 10,
+      },
+    ],
+    sitemap: 'https://pigeonlab.vercel.app/sitemap.xml',
+  };
+}
