27b17ce75a5131d2d667fb4aadd607d85797d0b3
JulioC090 <juliocziviani@gmail.com>
2025-09-06 00:19:14 -0300

    feat: improve seo with metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,9 +7,81 @@ import './index.css';
 
 export const metadata: Metadata = {
   title: "Pigeon's Lab",
-  description: "Welcome to the Pigeon's Lab",
+  description:
+    "Bem vindo ao Pigeon's Lab, onde desenvolvemos sites e aplicações com design acessível, escalável e humano com soluções digitais de alta qualidade.",
   icons: {
     icon: '/logo.svg',
+  },
+  applicationName: 'Pigeon Lab',
+  category: 'technology',
+  appLinks: {
+    web: { url: 'https://pigeonlab.vercel.app' },
+  },
+  keywords: [
+    // 🔹 Marca (variações)
+    'Pigeon Lab',
+    'pigeonlab',
+    'the pigeon lab',
+    'pigeons lab',
+    'pigeonslab',
+    'pigeon',
+    'lab',
+
+    // 🔹 Serviços principais
+    'desenvolvedor',
+    'programador',
+    'dev',
+    'fullstack',
+    'web',
+    'web development',
+    'freelancer',
+    'desenvolvedor de aplicações web',
+    'freelancer desenvolvimento web',
+    'criação de sites profissionais',
+    'desenvolvimento de site responsivo',
+    'manutenção de sites e sistemas',
+
+    // 🔹 Tecnologias
+    'nextjs',
+    'next js',
+    'next.js',
+    'react',
+    'reactjs',
+    'react.js',
+    'node',
+    'nodejs',
+    'node.js',
+    'typescript',
+    'type script',
+    'tailwind',
+    'tailwindcss',
+    'tailwind css',
+
+    // 🔹 Local
+    'Americana',
+    'São Paulo',
+    'desenvolvedor web Americana',
+    'criação de sites em Americana',
+    'programador freelance São Paulo',
+    'desenvolvedor fullstack Brasil',
+    'criação de sites SP',
+  ],
+  creator: 'JulioC090',
+  authors: [{ name: 'JulioC090', url: 'https://github.com/JulioC090' }],
+  openGraph: {
+    title: "Pigeon's Lab",
+    description: 'Transformando ideias em experiências digitais marcantes.',
+    url: 'https://pigeonlab.vercel.app',
+    siteName: "Pigeon's Lab",
+    images: [{ url: '/og-image.png', width: 1200, height: 630 }],
+    locale: 'pt_BR',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: "Pigeon's Lab",
+    description: 'Transformando ideias em experiências digitais marcantes.',
+    images: ['/og-image.png'],
   },
   alternates: {},
   verification: {
